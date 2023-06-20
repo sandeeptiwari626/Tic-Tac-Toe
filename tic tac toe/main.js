c7ef96190f7139dd0c7fe76fe6f37ac9c34a83c0
sandeepstudyportal <sandeep.studyportal@gmail.com>
2023-06-20 23:36:07 +0530

    initial commit

---
+++ tic tac toe/main.js
@@ -0,0 +1,90 @@
+//Declare variables
+let currentPlayer = 'X';
+let gameEnded = false;
+let gameMoves = ['', '', '', '', '', '', '', '', ''];
+
+//Get DOM
+document.addEventListener('DOMContentLoaded', () => {
+  const boxes = document.querySelectorAll('td');
+  //console.log(boxes);
+  const table = document.querySelector('table');
+  const gameScore = document.querySelector('#game-score');
+  let player = document.querySelector('#player');
+  let text =  document.querySelector('#text');
+
+  //add event listener to table boxes
+  boxes.forEach((box, index) =>{
+    /*console.log(box);
+    console.log(index);*/
+      box.addEventListener('click', ()=>{
+          if (!gameEnded && gameMoves[index] === '') {
+              gameMoves[index] = currentPlayer;
+              box.textContent = currentPlayer;
+              /*console.log(gameMoves);
+    console.log(box);*/
+              //check for winner
+              if (gameWin()) {
+                  table.style.display = 'none';
+                  gameScore.style.display = 'block';
+                  player.textContent = currentPlayer;
+                  text.textContent = 'WINNER';
+                  gameEnded = true;
+              }
+              //check for tie
+              else if (gameTie()) 
+              {
+                  table.style.display = 'none';
+                  gameScore.style.display = 'block';
+                  player.textContent = 'XO';
+                  text.textContent = 'DRAW';
+                  gameEnded = true;
+              } 
+              else
+              {
+                  currentPlayer = currentPlayer === 'X' ? '0' : 'X';
+              }
+          } 
+      });
+  });
+
+  //Check for winner function
+  const gameWin = ()=>{
+      const winningCombos = [
+      [0, 1, 2], [3, 4, 5], [6, 7, 8], // rows
+      [0, 3, 6], [1, 4, 7], [2, 5, 8], // columns
+      [0, 4, 8], [2, 4, 6] // diagonals
+    ];
+
+    return winningCombos.some(combo=>{
+        //console.log(combo);
+      return combo.every(index=>{
+        //console.log(index);
+          return gameMoves[index] === currentPlayer;
+      })
+    })
+  }
+
+  //Check for tie function
+  const gameTie = ()=>{
+      return gameMoves.every(move => {
+          return move !== '';
+      });
+  }
+
+  //Reset game function
+  const resetGame = ()=>{
+    table.style.display = 'block';
+    gameScore.style.display = 'none';
+    currentPlayer = 'X';
+    gameEnded = false;
+    gameMoves = ['', '', '', '', '', '', '', '', ''];
+    boxes.forEach(box => {
+      box.textContent = '';
+    });
+}
+
+
+  //Event listener for reset button
+  const resetButton = document.getElementById('reset-btn');
+  resetButton.addEventListener('click', resetGame);
+});
